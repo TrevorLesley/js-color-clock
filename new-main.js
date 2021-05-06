a8d0ef082986f4a3f3c6a2196ebd0b7d2006d10d
Trevor Lesley <tlesley96@gmail.com>
2021-05-06 19:27:09 -0400

    Esablished event listener.

--- new-main.js
+++ new-main.js
@@ -1,3 +1,16 @@
 const $clock = document.querySelector('.clock');
 const $clockDisplay = document.querySelector('.clock-display');
 const $clockProgressBar = document.querySelector('.clock-progress-bar');
+
+let isHovering = false;
+
+$clock.addEventListener('mouseover', function () {
+    isHovering = true;
+});
+
+$clock.addEventListener('mouseout', function () {
+    isHovering = false;
+});
+
+
+
